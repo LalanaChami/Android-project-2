5e07dc557bb4cc12bc4c880f65e7c688f40e71b1
Lalana Chamika <lalanachamika123@gmail.com>
2025-04-09 13:35:53 -0400

    test email address and subdomain format

---
+++ LocalTesting/app/src/test/java/lalana_nasif/examples/localtesting/EmailValidatorTest.kt
@@ -0,0 +1,18 @@
+package lalana_nasif.examples.localtesting
+
+import org.junit.Assert
+import org.junit.Test
+
+
+class EmailValidatorTest {
+
+    @Test
+    fun validEmail_basicFormat_returnsTrue() {
+        Assert.assertTrue(EmailValidator.isValidEmail("123@abc.com"))
+    }
+
+    @Test
+    fun validEmail_subdomainFormat_returnsTrue() {
+        Assert.assertTrue(EmailValidator.isValidEmail("123@abc.co.ca"))
+    }
+}
